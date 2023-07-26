800bc7ea41b99404e091fdd1206a0a023595bf6b
Felipe Ferraz <115377471+FelipeFerraz4@users.noreply.github.com>
2023-07-26 14:29:57 -0300

    adicionar elemento na lista

--- Listas/Lista Sequencial Estática/main.c
+++ Listas/Lista Sequencial Estática/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "Lista.h"
 
 int main() {
@@ -11,8 +12,17 @@ int main() {
     //if(full_list(list))
     int emptyList = empty_list(list);
     //if(empty_list(list))
+    Student student1;
+    student1.ID = 2;
+    strcmp(student1.name, "Sanessa");
+    student1.test1 = 9;
+    student1.test2 = 10;
+    student1.test3 = 8;
+    
+    add_list_element(list, student1);
 
     free_list(list);
+    printf("ponto %d", list->data[0].ID);
 
     return 0;
 }
