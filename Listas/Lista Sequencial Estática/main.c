6a866790f56ec9fc489c4e771d0340fd0e8bca3c
Felipe Ferraz <115377471+FelipeFerraz4@users.noreply.github.com>
2023-07-26 14:54:03 -0300

    Função que adiciona no começo da lista

--- Listas/Lista Sequencial Estática/main.c
+++ Listas/Lista Sequencial Estática/main.c
@@ -12,17 +12,25 @@ int main() {
     //if(full_list(list))
     int emptyList = empty_list(list);
     //if(empty_list(list))
-    Student student1;
-    student1.ID = 2;
-    strcmp(student1.name, "Sanessa");
+    Student student1, student2;
+    student1.ID = 1;
+    strcmp(student1.name, "Vanessa");
     student1.test1 = 9;
     student1.test2 = 10;
     student1.test3 = 8;
     
-    add_list_element(list, student1);
+    student2.ID = 2;
+    strcmp(student2.name, "Beatriz");
+    student2.test1 = 9.5;
+    student2.test2 = 9;
+    student2.test3 = 10;
+    
+    int result1 = add_list_element(list, student1);
+    int result2 = add_beginning_list(list, student2);
 
     free_list(list);
-    printf("ponto %d", list->data[0].ID);
+    printf("ponto %d\n", list->data[0].ID);
+    printf("ponto %d\n", list->data[1].ID);
 
     return 0;
 }
