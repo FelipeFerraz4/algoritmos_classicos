279f5044183c7f24da4d93196b4ac86934970e96
Felipe Ferraz <115377471+FelipeFerraz4@users.noreply.github.com>
2023-07-27 09:54:47 -0300

    Update Lista.c

--- Listas/Lista Sequencial Estática/Lista.c
+++ Listas/Lista Sequencial Estática/Lista.c
@@ -2,110 +2,107 @@
 #include <stdlib.h>
 #include "Lista.h"
 
-struct list{
+/*
+struct list_student{
     int size_element;
-    struct student data[SIZE_LIST];
-    /*
-    struct data{
-        struct student[SIZE_LIST];
-    }
-    */
+    Student data[SIZE_LIST];
 };
+*/
 
 List* create_list(){
-    List* list;
-    list = (List*) malloc(sizeof(struct list));
-    if(list != NULL){
-        list->size_element = 0;
+    List* list_student;
+    list_student = (List*) malloc(sizeof(struct list_student));
+    if(list_student != NULL){
+        list_student->size_element = 0;
     }
-    return list;
+    return list_student;
 }
 
-void free_list(List* list){
-    free(list);
+void free_list(List* list_student){
+    free(list_student);
 }
 
-int size_list(List* list){
-    if(list == NULL){
+int size_list(List* list_student){
+    if(list_student == NULL){
         return -1;
     }
     else{
-        return list->size_element;
+        return list_student->size_element;
     }
 }
 
-int full_list(List* list){
-    if(list == NULL){
+int full_list(List* list_student){
+    if(list_student == NULL){
         return -1;
     }
     else{
-        return (list->size_element == SIZE_LIST);
+        return (list_student->size_element == SIZE_LIST);
     }
 }
 
-int empty_list(List* list){
-    if(list == NULL){
+int empty_list(List* list_student){
+    if(list_student == NULL){
         return -1;
     }
     else{
-        return (list->size_element == 0);
+        return (list_student->size_element == 0);
     }
 }
 
 //adiciona elementos no final da lista
-int add_list_element(List* list, Student student){
-    if(list == NULL){
+int add_list_element(List* list_student, Student student){
+    if(list_student == NULL){
         return 0;
     }
-    
-    if(full_list(list) == 1){
+
+    if(full_list(list_student) == 1){
         return 0;
     }
-    
-    list->data[list->size_element] = student;
-    list->size_element++;
+
+    list_student->data[list_student->size_element] = student;
+    list_student->size_element++;
     return 1;
 }
 
 //adiciona elementos no inicio da lista
-int add_beginning_list(List* list, Student student){
-    if(list == NULL){
+int add_beginning_list(List* list_student, Student student){
+    if(list_student == NULL){
         return 0;
     }
-    
-    if(full_list(list)  == 1){
+
+    if(full_list(list_student)  == 1){
         return 0;
     }
-    
-    for(int i = list->size_element - 1; i >= 0; i--){
-        list->data[i+1] = list->data[i];
+
+    for(int i = list_student->size_element - 1; i >= 0; i--){
+        list_student->data[i+1] = list_student->data[i];
     }
-    
-    list->data[0] = student;
-    list->size_element++;
+
+    list_student->data[0] = student;
+    list_student->size_element++;
     return 1;
 }
 
 //adiciona elemento de forma ordenada na lista
-int add_sort_list(List* list, Student student){
-    if(list == NULL){
+int add_sort_list(List* list_student, Student student){
+    if(list_student == NULL){
         return 0;
     }
-    
-    if(full_list(list)  == 1){
+
+    if(full_list(list_student)  == 1){
         return 0;
     }
-    
+
     int i = 0;
-    while(i < list->size_element && list->data[i].ID < student.ID){
+    while(i < list_student->size_element && list_student->data[i].ID < student.ID){
         i++;
     }
-    
-    for(int j = list->size_element - 1; j >= i; j--){
-        list->data[j+1] = list->data[j];
+
+    for(int j = list_student->size_element - 1; j >= i; j--){
+        list_student->data[j+1] = list_student->data[j];
     }
-    
-    list->data[i] = student;
-    list->size_element++;
+
+    list_student->data[i] = student;
+    list_student->size_element++;
     return 1;
 }
