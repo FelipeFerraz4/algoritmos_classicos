ef493e6f9d170f7fd0131584d470ba410399f59a
Felipe Ferraz <115377471+FelipeFerraz4@users.noreply.github.com>
2023-08-06 00:15:14 -0300

    Criação dos TAD(struct) do programas

--- algoritmos relacionados à Listas/Heap Binária/Heap.h
+++ algoritmos relacionados à Listas/Heap Binária/Heap.h
@@ -1,6 +1,40 @@
 #ifndef HEAP_H_INCLUDED
 #define HEAP_H_INCLUDED
 
+#define SIZE_DEFAULT 30
+#define CAPACITY_PASSENGERS 10
+#define CAPACITY_PRODUCT 20
+
+typedef struct person{
+    int id;
+    char name[30];
+    int age;
+    char planet[30];
+}Person;
+
+typedef struct resources{
+    int id;
+    char name[30];
+    int quantity;
+} Resources;
+
+typedef struct spacecraft{
+    int spacecraft_id;
+    Person passengers[CAPACITY_PASSENGERS];
+    Resources product[CAPACITY_PRODUCT];
+}Spacecraft;
+
+typedef struct no{
+    int priority;
+    Spacecraft spacecraft;
+    struct no *left_child;
+    struct no *right_child;
+}No;
+
+typedef struct heap{
+    int size_heap;
+    No *no;
+}Heap;
 
 
 #endif // HEAP_H_INCLUDED
