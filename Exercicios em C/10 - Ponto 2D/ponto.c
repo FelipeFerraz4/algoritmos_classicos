21b11350b3211b3f6f589df8ee3cf3fca0eee633
Felipe Ferraz <115377471+FelipeFerraz4@users.noreply.github.com>
2023-08-08 09:23:29 -0300

    Update ponto.c

--- Exercicios em C/10 - Ponto 2D/ponto.c
+++ Exercicios em C/10 - Ponto 2D/ponto.c
@@ -4,7 +4,7 @@
 #include <math.h>
 #include "ponto.h"
 
-Point create_point(float coordinateX, float coordinateY, char symbol){
+Point create_point(int coordinateX, int coordinateY, char symbol){
     Point point;
 
     point.coordinateX = coordinateX;
@@ -13,6 +13,18 @@ Point create_point(float coordinateX, float coordinateY, char symbol){
     point.symbol = symbol;
 
     return point;
+}
+
+Line create_Line(Point point1, Point point2){
+    Line line;
+
+    line.point1.coordinateX = point1.coordinateX;
+    line.point1.coordinateY = point1.coordinateY;
+    line.point2.coordinateX = point2.coordinateX;
+    line.point2.coordinateY = point2.coordinateY;
+
+    return line;
+
 }
 
 float distance_between_points(Point point1, Point point2){
