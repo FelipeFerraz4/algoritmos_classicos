5290b8c46926f76a4e7eff101fa890a2c962f4c1
Felipe Ferraz <115377471+FelipeFerraz4@users.noreply.github.com>
2023-08-08 09:24:06 -0300

    Update main.c

--- Exercicios em C/10 - Ponto 2D/main.c
+++ Exercicios em C/10 - Ponto 2D/main.c
@@ -10,8 +10,8 @@ int main()
     Point duck;
     duck = create_point(4, 6, 'd');
 
-    printf("Coordenada X = %.2f\n", duck.coordinateX);
-    printf("Coordenada Y = %.2f\n", duck.coordinateY);
+    printf("Coordenada X = %d\n", duck.coordinateX);
+    printf("Coordenada Y = %d\n", duck.coordinateY);
 
     Point fish = create_point(4, 0, 'f');
     Point bread = create_point(0, 6, 'b');
@@ -21,6 +21,10 @@ int main()
 
     printf("Distancia para o peixe = %.2f\n", result1);
     printf("Distancia para o pao = %.2f\n", result2);
+
+    Line line_duck_fish = create_Line(duck, fish);
+    Line line_duck_bread = {duck, bread};
+
 
     return 0;
 }
