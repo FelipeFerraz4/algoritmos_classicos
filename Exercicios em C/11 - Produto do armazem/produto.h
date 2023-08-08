ccbca2527632ccbadb826254d3284afd8140de85
Felipe Ferraz <115377471+FelipeFerraz4@users.noreply.github.com>
2023-08-08 09:55:02 -0300

    Create produto.h

---
+++ Exercicios em C/11 - Produto do armazem/produto.h
@@ -0,0 +1,11 @@
+#ifndef PRODUTO_H_INCLUDED
+#define PRODUTO_H_INCLUDED
+
+typedef struct product{
+    int id;
+    char name[50];
+    int quantity;
+    float price;
+} Product;
+
+#endif // PRODUTO_H_INCLUDED
