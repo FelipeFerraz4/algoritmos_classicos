a4bc742018cf46c903b6584244773c0105a658d5
Felipe Ferraz <115377471+FelipeFerraz4@users.noreply.github.com>
2023-08-08 09:24:34 -0300

    Update ponto.h

--- Exercicios em C/10 - Ponto 2D/ponto.h
+++ Exercicios em C/10 - Ponto 2D/ponto.h
@@ -2,11 +2,22 @@
 #define PONTO_H_INCLUDED
 
 typedef struct point{
-    float coordinateX, coordinateY;
+    int coordinateX, coordinateY;
     char symbol;
 } Point;
 
-Point create_point(float coordinateX, float coordinateY, char symbol);
+typedef struct line{
+    Point point1;
+    Point point2;
+}Line;
+
+typedef struct plane{
+    int size_plene;
+    Point point[3];
+}Plane;
+
+Point create_point(int coordinateX, int coordinateY, char symbol);
+Line create_Line(Point point1, Point point2);
 float distance_between_points(Point Point1, Point Point2);
 
 #endif // PONTO_H_INCLUDED
