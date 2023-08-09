59af8831e1ad2a292c43b168839f9f51285cd5cd
Felipe Ferraz <115377471+FelipeFerraz4@users.noreply.github.com>
2023-08-09 09:30:09 -0300

    Create main.c

---
+++ algoritmos relacionados à Listas/Lista dinâmica duplamente encadeada/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    List* student_list;
+    student_list = create_list();
+    
+    int size = size_list(student_list);
+    printf("Tamanho da list: %d\n", size);
+    
+    int empty = empty_list(student_list);
+    printf("Lista vazia: %d\n", empty);
+    
+    printf("Hello world");
+    
+    free_list(student_list);
+
+    return 0;
+}
