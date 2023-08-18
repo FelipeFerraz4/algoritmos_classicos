fbbe8fefb9a54b9245c81219ae7be4a57be9d7c5
Felipe Ferraz <115377471+FelipeFerraz4@users.noreply.github.com>
2023-08-18 11:35:57 -0300

    Soma de N números ao cubo

---
+++ Exercicios em C/18 - Soma de N números Recursivo/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int soma_numeros(int number){
+    if(number == 0){
+        return 0;
+    }
+    else{
+        return number + soma_numeros(number-1);
+    }
+}
+
+int main()
+{
+    int number;
+    printf("Digite um numero: \n");
+    scanf("%d", &number);
+
+    number = soma_numeros(number);
+    number = number * number *number;
+
+    printf("A soma = %d\n", number);
+
+    return 0;
+}
